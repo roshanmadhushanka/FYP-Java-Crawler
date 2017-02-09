d941ef6b576772d0545c005c34d2ca70f4d4d604
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2017-02-10 04:03:40 +0530

    Added FileHandler class

---
+++ src/main/java/io/FileHandler.java
@@ -0,0 +1,26 @@
+package io;
+
+/**
+ * Created by Roshan on 2/10/2017.
+ */
+public class FileHandler {
+    public void readHTML(String path){
+        /*
+        Read HTML from file
+
+        path : location of the file
+         */
+
+    }
+
+    public void writeHTML(String data, String path){
+        /*
+        Write HTML to a file
+
+        data : HTML data
+        path : location where to written
+         */
+
+
+    }
+}
